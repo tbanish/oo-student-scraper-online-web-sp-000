2ba9a7bd55676262916d9d67651e664affb6a5b6
Tom Banish <tombanish@gmail.com>
2020-04-27 18:24:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.attributes_hash.each {|key, value| self.send(("#{key}="), value)}
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
     binding.pry
   end
 
