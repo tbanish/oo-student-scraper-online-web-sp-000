661edd1319f303cbde92c12a7793ca1d47dd853d
Tom Banish <tombanish@gmail.com>
2020-04-27 17:55:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,9 +32,13 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
+    
     container = doc.css(".social-icon-container a").collect do |icon|
       icon.attribute("href").value
     end
+    
+    
+    
     
     binding.pry
   end
